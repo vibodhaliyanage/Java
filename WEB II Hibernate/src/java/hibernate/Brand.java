52e75fba432419e00322006832053eb5d2a449ce
vibodha liyanage <126347779+vibodhaliyanage@users.noreply.github.com>
2025-07-09 12:31:51 +0530

    now

---
+++ WEB II Hibernate/src/java/hibernate/Brand.java
@@ -0,0 +1,44 @@
+package hibernate;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="brand")
+
+public class Brand implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name = "name",length = 20,nullable = false)
+    private String name;
+
+    public Brand() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
